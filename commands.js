0f8c8e37b560fa53354bc6022179739ade49dc8b
Mazen <mazenmagdy96@yahoo.com>
2017-01-17 21:05:06 +0200

    Back

--- commands.js
+++ commands.js
@@ -664,92 +664,92 @@ var commands = {
 				}
 		}
 	},
-	testMusic: {
-		name: 'Test Music',
-		usage: PREFIX + 'testMusic',
-		description: 'For Testing Music',
-		hidden: true,
-		executor: function(message) {
-			if (!checkOwner(message)) {
-				message.channel.sendMessage('You don\'t have enough juice!');
-				return;
-			}
-			var member = message.member;
-			if (!member) {
-				message.channel.sendMessage('Works Only in guild!');
-				return;
-			}
-			musicChannel = member.voiceChannel;
-			if (!musicChannel) {
-				message.channel.sendMessage('You Must be in a voice channel to use this.');
-				return;
-			}
-			if (!musicChannel.joinable) {
-				message.channel.sendMessage('You Must be in a joinable voice channel to use this.');
-				return;
-			}
-			var stored = message.content.split(' ')[1] == null;
-			musicChannel.join().then(function(connection) {
-				musicConnection = connection;
-				if (stored) {
-					var stream = ytdl('https://www.youtube.com/watch?v=6Yr_gDLcX7Q', {
-						quality: 'lowest',
-						filter: 'audioonly'
-					});
-					musicDispatcher = connection.playStream(stream);
-				} else {
-					musicDispatcher = connection.playFile('./Billie Eilish - Ocean Eyes.mp3');
-				}
-			}).catch(console.log);
-		}
-	},
-	//TODO : handle corner cases
-	testPause: {
-		name: 'Test Pause',
-		usage: PREFIX + 'testPause',
-		description: 'For Testing Pause',
-		hidden: true,
-		executor: function(message) {
-			if (!checkOwner(message)) {
-				message.channel.sendMessage('You don\'t have enough juice.');
-				return;
-			}
-			if (musicDispatcher)
-				musicDispatcher.pause();
-		}
-	},
-	testResume: {
-		name: 'Test Resume',
-		usage: PREFIX + 'testResume',
-		description: 'For Testing Resume',
-		hidden: true,
-		executor: function(message) {
-			if (!checkOwner(message)) {
-				message.channel.sendMessage('You don\'t have enough juice.');
-				return;
-			}
-			if (musicDispatcher && musicDispatcher.paused)
-				musicDispatcher.resume();
-		}
-	},
-	testEnd: {
-		name: 'Test End',
-		usage: PREFIX + 'testEnd',
-		description: 'For Testing End',
-		hidden: true,
+	// testMusic: {
+	// 	name: 'Test Music',
+	// 	usage: PREFIX + 'testMusic',
+	// 	description: 'For Testing Music',
+	// 	hidden: true,
+	// 	executor: function(message) {
+	// 		if (!checkOwner(message)) {
+	// 			message.channel.sendMessage('You don\'t have enough juice!');
+	// 			return;
+	// 		}
+	// 		var member = message.member;
+	// 		if (!member) {
+	// 			message.channel.sendMessage('Works Only in guild!');
+	// 			return;
+	// 		}
+	// 		musicChannel = member.voiceChannel;
+	// 		if (!musicChannel) {
+	// 			message.channel.sendMessage('You Must be in a voice channel to use this.');
+	// 			return;
+	// 		}
+	// 		if (!musicChannel.joinable) {
+	// 			message.channel.sendMessage('You Must be in a joinable voice channel to use this.');
+	// 			return;
+	// 		}
+	// 		var stored = message.content.split(' ')[1] == null;
+	// 		musicChannel.join().then(function(connection) {
+	// 			musicConnection = connection;
+	// 			if (stored) {
+	// 				var stream = ytdl('https://www.youtube.com/watch?v=6Yr_gDLcX7Q', {
+	// 					quality: 'lowest',
+	// 					filter: 'audioonly'
+	// 				});
+	// 				musicDispatcher = connection.playStream(stream);
+	// 			} else {
+	// 				musicDispatcher = connection.playFile('./Billie Eilish - Ocean Eyes.mp3');
+	// 			}
+	// 		}).catch(console.log);
+	// 	}
+	// },
+	// //TODO : handle corner cases
+	// testPause: {
+	// 	name: 'Test Pause',
+	// 	usage: PREFIX + 'testPause',
+	// 	description: 'For Testing Pause',
+	// 	hidden: true,
+	// 	executor: function(message) {
+	// 		if (!checkOwner(message)) {
+	// 			message.channel.sendMessage('You don\'t have enough juice.');
+	// 			return;
+	// 		}
+	// 		if (musicDispatcher)
+	// 			musicDispatcher.pause();
+	// 	}
+	// },
+	// testResume: {
+	// 	name: 'Test Resume',
+	// 	usage: PREFIX + 'testResume',
+	// 	description: 'For Testing Resume',
+	// 	hidden: true,
+	// 	executor: function(message) {
+	// 		if (!checkOwner(message)) {
+	// 			message.channel.sendMessage('You don\'t have enough juice.');
+	// 			return;
+	// 		}
+	// 		if (musicDispatcher && musicDispatcher.paused)
+	// 			musicDispatcher.resume();
+	// 	}
+	// },
+	// testEnd: {
+	// 	name: 'Test End',
+	// 	usage: PREFIX + 'testEnd',
+	// 	description: 'For Testing End',
+	// 	hidden: true,
 
-		executor: function(message) {
-			if (!checkOwner(message)) {
-				message.channel.sendMessage('You don\'t have enough juice.');
-				return;
-			}
-			if (musicDispatcher) {
-				musicDispatcher.end();
-				musicConnection.disconnect();
-				musicChannel.leave();
-			}
-		}
-	},
+	// 	executor: function(message) {
+	// 		if (!checkOwner(message)) {
+	// 			message.channel.sendMessage('You don\'t have enough juice.');
+	// 			return;
+	// 		}
+	// 		if (musicDispatcher) {
+	// 			musicDispatcher.end();
+	// 			musicConnection.disconnect();
+	// 			musicChannel.leave();
+	// 		}
+	// 	}
+	// },
 };
 
 module.exports = {
