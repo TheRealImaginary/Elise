a6c01e3a7c2401448688c9298ff32885fc7de480
Mazen <mazennnnnnnnn@gmail.com>
2017-06-16 16:40:26 +0200

    Added Documentation to some Utility Functions

--- util/randomizer.js
+++ util/randomizer.js
@@ -1,7 +1,18 @@
+/**
+ * Returns a random number between low and high Exclusive.
+ * @param {number} low - Lower Bound.
+ * @param {number} high - Upper Bound.
+ * @returns {number} The Random Number.
+ */
 function getRandom(low, high) {
   return Math.floor((Math.random() * (high - low)) + low);
 }
 
+/**
+ * Returns a random element from an array.
+ * @param {Array<*>|string} list - List from which an element must be chosen.
+ * @returns {?any} The Random Element.
+ */
 function randomizer(list) {
   if (typeof list === 'string') {
     list = list.split(',');
