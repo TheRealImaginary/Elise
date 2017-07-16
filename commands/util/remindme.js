172476ecc01f5a4c9e06fe58c458f6dc6be8a260
Mazen <mazennnnnnnnn@gmail.com>
2017-07-16 22:37:16 +0200

    Sherlock cannot be installed on heroku disabling `remindme` command till I investigate

--- commands/util/remindme.js
+++ commands/util/remindme.js
@@ -1,45 +1,45 @@
-const Sherlock = require('Sherlock');
-const moment = require('moment');
-const { Command } = require('discord.js-commando');
+// const Sherlock = require('Sherlock');
+// const moment = require('moment');
+// const { Command } = require('discord.js-commando');
 
-module.exports = class RemindMeCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'remindme',
-      aliases: ['reminder'],
-      autoAliases: false,
-      group: 'util',
-      memberName: 'remindme',
-      description: 'Sends a DM reminding a user of something after a specific amount of time',
-      args: [{
-        key: 'duration',
-        prompt: 'When do you want me to remind you ?',
-        type: 'string',
-        validate(duration) {
-          const parsed = Sherlock.parse(duration);
-          if (!parsed.startDate || new Date(parsed.startDate) - Date.now() <= 0) {
-            return 'I am sorry that was not a valid date format!';
-          }
-          return true;
-        },
-        parse(duration) {
-          return Sherlock.parse(duration).startDate;
-        },
-      }, {
-        key: 'content',
-        prompt: 'What would you like me to remind you of ?',
-        default: '',
-        type: 'string',
-      }],
-    });
-  }
+// module.exports = class RemindMeCommand extends Command {
+//   constructor(client) {
+//     super(client, {
+//       name: 'remindme',
+//       aliases: ['reminder'],
+//       autoAliases: false,
+//       group: 'util',
+//       memberName: 'remindme',
+//       description: 'Sends a DM reminding a user of something after a specific amount of time',
+//       args: [{
+//         key: 'duration',
+//         prompt: 'When do you want me to remind you ?',
+//         type: 'string',
+//         validate(duration) {
+//           const parsed = Sherlock.parse(duration);
+//           if (!parsed.startDate || new Date(parsed.startDate) - Date.now() <= 0) {
+//             return 'I am sorry that was not a valid date format!';
+//           }
+//           return true;
+//         },
+//         parse(duration) {
+//           return Sherlock.parse(duration).startDate;
+//         },
+//       }, {
+//         key: 'content',
+//         prompt: 'What would you like me to remind you of ?',
+//         default: '',
+//         type: 'string',
+//       }],
+//     });
+//   }
 
-  run(message, { content, duration }) {
-    duration = new Date(duration) - Date.now();
-    setTimeout(() => {
-      message.author.send(`You asked to be reminded of ${content && content.length > 0 ? content : 'something'
-        }`);
-    }, duration);
-    message.say(`I will remind you in ${moment.duration(duration, 'milliseconds').humanize()}!`);
-  }
-};
+//   run(message, { content, duration }) {
+//     duration = new Date(duration) - Date.now();
+//     setTimeout(() => {
+//       message.author.send(`You asked to be reminded of ${content && content.length > 0 ? content : 'something'
+//         }`);
+//     }, duration);
+//     message.say(`I will remind you in ${moment.duration(duration, 'milliseconds').humanize()}!`);
+//   }
+// };
